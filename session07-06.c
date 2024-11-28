5ddf3588f73d8afd7a3aebe8e53448128683c405
Huunhat123 <huunhat20052006@gmail.com>
2024-11-28 12:11:25 +0700

    session07-06.c

--- session07-06.c
+++ session07-06.c
@@ -3,8 +3,7 @@
 int main()
 {
 	// khai bao ham 
-	int num[5]={1,5,2,7,3};
-	int i; 
+	int num[5]={4,4,4,4,4};
 
     // in va ket thuc
     for (int i = 0; i < 5; i++) {
@@ -12,14 +11,10 @@ int main()
 	num[i]	+=2; 
 	} 
 	else{
-		for (int i = 0; i < 5; i++) {
-			if(num[i]%2==0){
-			num[i] +=3; 
-			}
-    }
-	} 
+	num[i] +=3; 
+	}
 	printf("%d ",num[i]);
-}
+    }	 
 	return 0;
 
  }
